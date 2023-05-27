1b32f2b8a2319576c8cb8e6fc90447bce88582a3
mahabubr <mahabub.r753@gmail.com>
2023-05-27 23:26:24 +0600

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+	return (
+		<header className='w-full flex justify-center items-center flex-col'>
+			<nav className='flex justify-between items-center w-full mb-10 pt-3'>
+				<img
+					src={logo}
+					alt=''
+					className='w-28 object-contain'
+				/>
+				<button
+					type='button'
+					onClick={() => window.open("https://github.com/mahabubr")}
+					className='black_btn'>
+					Github
+				</button>
+			</nav>
+			<h1 className='head_text'>
+				Summarized Article With <br className='max-md:hidden' />{" "}
+				<span className='orange_gradient'>OPEN AI GPT-4</span>
+			</h1>
+			<h2 className='desc'>
+				Simplify your reading with summarize, an open source article summarizer
+				that transform lengthy article into clear and concise summaries
+			</h2>
+		</header>
+	);
+};
+
+export default Hero;
